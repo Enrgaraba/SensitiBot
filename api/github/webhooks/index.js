1b0c22c5893862b79aa0f617d9edd3a905d5a994
Enrgaraba <enrgaraba@alum.us.es>
2025-04-07 22:20:24 +0200

    Netlify deploy intento 1

--- api/github/webhooks/index.js
+++
@@ -1,8 +0,0 @@
-import { createNodeMiddleware, createProbot } from "probot";
-
-const app = require("../../../index");
-
-module.exports = createNodeMiddleware(app, {
- probot: createProbot(),
- webhooksPath: "/api/github/webhooks",
-});
